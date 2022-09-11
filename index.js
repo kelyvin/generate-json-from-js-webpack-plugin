4d4f098cc4e253c4748c9c8089e27c14a9e75e1a
Chad Bailey <chadbailey904@gmail.com>
2022-09-11 14:57:45 -0500

    Auto-formatted with prettier (w/ trailing commas)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const path = require('path')
-const chalk = require('chalk')
+const path = require("path");
+const chalk = require("chalk");
 
 const defaultJsonOptions = {
   replacer: null,
-  space: 2
-}
+  space: 2,
+};
 
-function GenerateJsonFromJsPlugin (config = {}) {
+function GenerateJsonFromJsPlugin(config = {}) {
   Object.assign(this, {
     ...config,
     filePath: config.path,
@@ -14,55 +14,56 @@ function GenerateJsonFromJsPlugin (config = {}) {
     data: config.data || [],
     options: {
       ...defaultJsonOptions,
-      ...config.options
-    }
-  })
+      ...config.options,
+    },
+  });
 
-  this.plugin = 'GenerateJsonFromJsPlugin'
+  this.plugin = "GenerateJsonFromJsPlugin";
 }
 
-GenerateJsonFromJsPlugin.prototype.apply = function apply (compiler) {
+GenerateJsonFromJsPlugin.prototype.apply = function apply(compiler) {
   const emit = (compilation, callback) => {
-    const fullFilePath = path.resolve(compiler.context, this.filePath)
+    const fullFilePath = path.resolve(compiler.context, this.filePath);
 
     // Adding this causes a "rebuild" when the template file changes and indicate when the file has changed
-    compilation.fileDependencies.add(fullFilePath)
-    process.stdout.write(`\n${chalk.green('Rebuilding ')}${this.filename}\n`)
+    compilation.fileDependencies.add(fullFilePath);
+    process.stdout.write(`\n${chalk.green("Rebuilding ")}${this.filename}\n`);
 
-    const jsModule = require(fullFilePath)
-    let jsonValue = null
+    const jsModule = require(fullFilePath);
+    let jsonValue = null;
 
-    if (typeof jsModule === 'function') {
-      jsonValue = jsModule(this.data)
+    if (typeof jsModule === "function") {
+      jsonValue = jsModule(this.data);
     } else if (jsModule instanceof Array) {
-      jsonValue = [
-        ...jsModule,
-        ...this.data,
-      ]
-    } else if (typeof jsModule === 'object') {
+      jsonValue = [...jsModule, ...this.data];
+    } else if (typeof jsModule === "object") {
       jsonValue = {
         ...jsModule,
-        ...this.data
-      }
+        ...this.data,
+      };
     }
 
     if (jsonValue) {
-      const json = JSON.stringify(jsonValue, this.options.replacer, this.options.space)
+      const json = JSON.stringify(
+        jsonValue,
+        this.options.replacer,
+        this.options.space
+      );
 
       compilation.assets[this.filename] = {
         source: () => json,
-        size: () => json.length
-      }
+        size: () => json.length,
+      };
     }
 
-    callback()
-  }
+    callback();
+  };
 
   if (compiler.hooks) {
-    compiler.hooks.emit.tapAsync(this.plugin, emit)
+    compiler.hooks.emit.tapAsync(this.plugin, emit);
   } else {
-    compiler.plugin('emit', emit)
+    compiler.plugin("emit", emit);
   }
-}
+};
 
-module.exports = GenerateJsonFromJsPlugin
+module.exports = GenerateJsonFromJsPlugin;
