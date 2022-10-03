05f6238c7c973b4b269b4aa21352f286f43cd245
Chad Bailey <chadbailey904@gmail.com>
2022-10-02 22:55:23 -0500

    Fixes #5 (#6)
    * Auto-formatted with prettier (w/ trailing commas)
    
    * Eliminated chalk dependency
    
    * Abstracted plugin name into const
    
    * Refactored into real JS class
    
    * Replaced hooks.emit.tapAsync with hooks.thisCompilation.tap - fixes #5
    
    * Implemented array fix
    
    - Found on this commit:
    https://github.com/kelyvin/generate-json-from-js-webpack-plugin/commit/eaf804f37a3c19640682c010f6d3eb001667036f
    
    - Credit to ImanMh's fork:
    https://github.com/ImanMh/generate-json-from-js-webpack-plugin

--- index.js
+++ index.js
@@ -1,68 +1,70 @@
-const path = require('path')
-const chalk = require('chalk')
+const PLUGIN_NAME = "GenerateJsonFromJsPlugin";
+const path = require("path");
 
 const defaultJsonOptions = {
   replacer: null,
-  space: 2
-}
+  space: 2,
+};
 
-function GenerateJsonFromJsPlugin (config = {}) {
-  Object.assign(this, {
-    ...config,
-    filePath: config.path,
-    filename: config.filename,
-    data: config.data || [],
-    options: {
-      ...defaultJsonOptions,
-      ...config.options
-    }
-  })
-
-  this.plugin = 'GenerateJsonFromJsPlugin'
-}
-
-GenerateJsonFromJsPlugin.prototype.apply = function apply (compiler) {
-  const emit = (compilation, callback) => {
-    const fullFilePath = path.resolve(compiler.context, this.filePath)
-
-    // Adding this causes a "rebuild" when the template file changes and indicate when the file has changed
-    compilation.fileDependencies.add(fullFilePath)
-    process.stdout.write(`\n${chalk.green('Rebuilding ')}${this.filename}\n`)
-
-    const jsModule = require(fullFilePath)
-    let jsonValue = null
-
-    if (typeof jsModule === 'function') {
-      jsonValue = jsModule(this.data)
-    } else if (jsModule instanceof Array) {
-      jsonValue = [
-        ...jsModule,
-        ...this.data,
-      ]
-    } else if (typeof jsModule === 'object') {
-      jsonValue = {
-        ...jsModule,
-        ...this.data
-      }
-    }
-
-    if (jsonValue) {
-      const json = JSON.stringify(jsonValue, this.options.replacer, this.options.space)
-
-      compilation.assets[this.filename] = {
-        source: () => json,
-        size: () => json.length
-      }
-    }
-
-    callback()
+class GenerateJsonFromJsPlugin {
+  constructor(config = {}) {
+    Object.assign(this, {
+      ...config,
+      filePath: config.path,
+      filename: config.filename,
+      data: config.data || [],
+      options: {
+        ...defaultJsonOptions,
+        ...config.options,
+      },
+    });
+    this.plugin = PLUGIN_NAME;
   }
+  apply(compiler) {
+    const emit = (compilation) => {
+      const fullFilePath = path.resolve(compiler.context, this.filePath);
 
-  if (compiler.hooks) {
-    compiler.hooks.emit.tapAsync(this.plugin, emit)
-  } else {
-    compiler.plugin('emit', emit)
+      // Adding this causes a "rebuild" when the template file changes and indicate when the file has changed
+      compilation.fileDependencies.add(fullFilePath);
+      process.stdout.write(`\n${green("Rebuilding ")}${this.filename}\n`);
+
+      const jsModule = require(fullFilePath);
+      let jsonValue = null;
+
+      if (typeof jsModule === "function") {
+        jsonValue = jsModule(this.data);
+      } else if (jsModule instanceof Array) {
+        jsonValue = [...jsModule, ...this.data];
+      } else if (typeof jsModule === "object") {
+        jsonValue = {
+          ...jsModule,
+          ...this.data,
+        };
+      }
+
+      if (jsonValue) {
+        const json = JSON.stringify(
+          jsonValue,
+          this.options.replacer,
+          this.options.space
+        );
+
+        compilation.assets[this.filename] = {
+          source: () => json,
+          size: () => json.length,
+        };
+      }
+    };
+
+    if (compiler.hooks) {
+      compiler.hooks.thisCompilation.tap(this.plugin, emit);
+    } else {
+      compiler.plugin("emit", emit);
+    }
   }
 }
+module.exports = GenerateJsonFromJsPlugin;
 
-module.exports = GenerateJsonFromJsPlugin
+function green(str) {
+  return "\u001b[1m\u001b[32m" + str + "\u001b[39m\u001b[22m";
+}
